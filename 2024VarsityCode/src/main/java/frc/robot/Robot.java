e418aa790befba45b1255160031f0c2e030428e8
matth <hallmatthewd12@gmail.com>
2024-01-30 18:26:36 -0500

    Removed AdvantageKit, setup logging, and port forwarded the radio

--- 2024VarsityCode/src/main/java/frc/robot/Robot.java
+++ 2024VarsityCode/src/main/java/frc/robot/Robot.java
@@ -4,9 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.lib.util.LogOrDash;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -28,6 +30,10 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    LogOrDash.setupLogging();
+    for(int port = 5800; port <= 5805; port++){
+      PortForwarder.add(port, "limelight.local", port);
+    }
   }
 
   /**
