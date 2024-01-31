e05ec7eb05ead1c1a15b6e7d20218c5425af94bf
matth <hallmatthewd12@gmail.com>
2024-01-30 22:41:45 -0500

    Setup index subsystem and commands

---
+++ 2024VarsityCode/src/main/java/frc/robot/subsystems/Index.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/* Desired Behavior:
+ * When the toggle button is pressed to turn the intake on, it also starts the index motor.
+ * The motor will run until the break beam is broken, and this will then stop both the index
+ * and intake motors. Will be triggered with the same groups as intake. Index should just
+ * always run at its set speed, and commands just change the set speed
+ */
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Index extends SubsystemBase {
+  private CANSparkMax indexMotor;
+  private DigitalInput indexEye;
+
+  private double indexSpeed = 0.0;
+  public static final double DEFAULT_SPEED = 0.3;
+  /** Creates a new Index. */
+  public Index() {
+    indexMotor = new CANSparkMax(Constants.Index.indexCAN, MotorType.kBrushless);
+    indexEye = new DigitalInput(Constants.Index.indexDIO);
+  }
+
+  public boolean noteStored(){
+    return !indexEye.get();
+  }
+
+  public void setIndexSpeed(double speed){
+    indexSpeed = speed;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    indexMotor.set(indexSpeed);
+  }
+}
