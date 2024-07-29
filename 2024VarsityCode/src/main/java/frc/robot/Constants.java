cfc178139d129943e9e46f985fa07d16f4ca341d
THatMustacheCat <98564568+THatMustacheCat@users.noreply.github.com>
2024-07-28 20:47:03 -0400

    Rainbow Rumble alterations

--- 2024VarsityCode/src/main/java/frc/robot/Constants.java
+++ 2024VarsityCode/src/main/java/frc/robot/Constants.java
@@ -86,12 +86,12 @@ public final class Constants {
 
         /* Swerve Current Limiting */
         public static final int angleContinuousCurrentLimit = 20;
-        public static final int anglePeakCurrentLimit = 400;
+        public static final int anglePeakCurrentLimit = 40;
         public static final double anglePeakCurrentDuration = 0.1;
         public static final boolean angleEnableCurrentLimit = true;
 
         public static final int driveContinuousCurrentLimit = 35;
-        public static final int drivePeakCurrentLimit = 60;
+        public static final int drivePeakCurrentLimit = 50;
         public static final double drivePeakCurrentDuration = 0.1;
         public static final boolean driveEnableCurrentLimit = true;
 
